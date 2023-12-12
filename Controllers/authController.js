a62315dc2e03a57e4fdd442f9c35126d2243267d
Kasaija <kasaijak@gmail.com>
2023-12-12 19:50:28 +0300

    implement signin and register functionalities

--- Controllers/authController.js
+++ Controllers/authController.js
@@ -1,6 +1,26 @@
 const User = require("./../Models/userModel");
+const jwt = require("jsonwebtoken");
 
-exports.signIn = async (req, res) => {};
+const tokenGen = (payload) => {
+  return jwt.sign(payload, process.env.S_KEY, {
+    expiresIn: process.env.MAX_AGE,
+  });
+};
+
+exports.signIn = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) throw new Error("Bad request");
+    const token = tokenGen({ name: user.name, id: user._id });
+    res.status(200).json({ user, token });
+  } catch (error) {
+    res.status(400).json({
+      status: "Fail",
+      message: error.message,
+    });
+  }
+};
 
 exports.signOut = async (req, res) => {};
 

977ebf4e1b33044f51c1f87910439a270b220b35
Kasaija <kasaijak@gmail.com>
2023-12-16 20:04:22 +0300

    update guard/protection logic and logic to update/delete user

--- Routes/userRouter.js
+++ Routes/userRouter.js
@@ -4,11 +4,11 @@ const authController = require("./../Controllers/authController");
 
 router.param("user_id", userController.checkId);
 
-router.route("/").get(userController.getUsers);
+router.route("/").get(authController.protect, userController.getUsers);
 router
   .route("/:user_id")
-  .get(userController.getUser)
-  .patch(authController.isOwnProfile, userController.updateUser)
-  .delete(authController.isOwnProfile, userController.deleteUser);
+  .get(authController.protect, userController.getUser)
+  .patch(authController.protect, userController.updateUser)
+  .delete(authController.protect, userController.deleteUser);
 
 module.exports = router;

3a4a323acc6642230bf7dd2672f1544422edc7ef
Kasaija <kasaijak@gmail.com>
2023-12-18 11:13:01 +0300

    add user log out logic., store jwt in a cookie for ease of management

--- Controllers/authController.js
+++ Controllers/authController.js
@@ -1,24 +1,20 @@
-const User = require("./../Models/userModel");
-const jwt = require("jsonwebtoken");
-const util = require("util");
-
-// const tokenGen = (payload) => {
-//   return jwt.sign(payload, process.env.S_KEY, {
-//     expiresIn: process.env.MAX_AGE,
-//   });
-// };
+const User = require("./../Models/userModel"),
+  jwt = require("jsonwebtoken"),
+  util = require("util");
 
 exports.isOwner = (req, res, next) => {
   try {
-    //credentials and user objects are defined in the 'protect' middleware function
-    const credentials = req.credentials;
-    const user = req.user;
-    const owner = user && credentials && credentials.id === user._id;
+    //The 'credentials', and 'user' objects are defined in the 'protect' middleware function
+    const credentials = req.credentials,
+      user = req.user,
+      owner = user && credentials && credentials.id === user._id;
+
     if (!owner) {
       throw new Error(
         "You are not authorized to perform this action. Only profile owners can update/ delete"
       );
     }
+
     next();
   } catch (error) {
     res.status(400).json({
@@ -48,17 +44,19 @@ exports.signIn = async (req, res) => {
     //create a cookie definition
     let options = {
       maxAge: 1000 * 60 * 20, // would expire in 20minutes
-      httpOnly: true, // The cookie is only accessible by the web server
-      secure: true,
-      sameSite: "None",
+      httpOnly: true, // used during development
+      secure: process.env.NODE_ENV === "production", //used during production
+      //sameSite: "None",
     };
 
     user.password = undefined;
-    res.cookie("SessionID", token, options); // set the token to respons header, so that the client sends it back on each subsequent request
-    res.status(200).json({
-      status: "Success",
-      message: "You have successfully logged in",
-    });
+    res
+      .cookie("token", token, options) // set the token to respons header, so that the client sends it back on each subsequent request
+      .status(200)
+      .json({
+        status: "Success",
+        message: "You have successfully logged in",
+      });
   } catch (error) {
     res.status(400).json({
       status: "Fail",
@@ -67,7 +65,12 @@ exports.signIn = async (req, res) => {
   }
 };
 
-exports.signOut = async (req, res) => {};
+exports.signOut = async (req, res) => {
+  res.clearCookie("token").status(200).json({
+    status: "Success",
+    message: "Successfully logged out 😏 🍀",
+  });
+};
 
 exports.register = async (req, res) => {
   try {
@@ -89,27 +92,25 @@ exports.register = async (req, res) => {
 exports.protect = async (req, res, next) => {
   try {
     //check if token exists
-    let token = req.headers.cookie ? req.headers.cookie : null;
-    console.log(req.headers);
+    let token = req.cookies.token ? req.cookies.token : null;
     if (!token) throw new Error("You are not authenticated. Please login");
 
     //validate the received token
-    if (token.startsWith("authID")) {
-      token = await util.promisify(jwt.verify)(
-        token.split(" ")[1],
-        process.env.S_KEY
-      );
-    }
+    //for jwt stored in a cookie named token
+    token = await util.promisify(jwt.verify)(token, process.env.S_KEY);
 
     //check if user exists
     const user = await User.findById(token.id).select("-password");
     if (!user) throw new Error("The user does not exist");
 
     //check if user changed password after issuance of token (i.e after login)
-    if (await user.isUserUpdated(token.iat))
-      throw new Error(
-        "Your User Profile was recently updated. Please login again"
-      );
+    if (await user.isUserUpdated(token.iat)) {
+      res.clearCookie("token").json({
+        status: "Success",
+        message: "Your User Profile was recently updated and you have been logged out. Please login again",
+      });
+      return;
+    }
 
     //Add decoded token to req object
     req.credentials = token;
